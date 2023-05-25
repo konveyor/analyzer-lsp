1a7278d1167699ac4f567bb09d3c10270ceae4df
Chanakya-TS <66557279+Chanakya-TS@users.noreply.github.com>
2023-05-25 15:53:19 -0500

    :bug: stopped duplication of incidents while creating violations (#161)
    fixes issue #144
    
    ---------
    
    Signed-off-by: Chanakya Thirumala Setty <chanakyats@gmail.com>
    Co-authored-by: Chanakya Thirumala Setty <cthiruma@localhost-live.attlocal.net>

--- engine/engine.go
+++ engine/engine.go
@@ -359,6 +359,7 @@ func processRule(ctx context.Context, rule Rule, ruleCtx ConditionContext, log l
 func (r *ruleEngine) createViolation(conditionResponse ConditionResponse, rule Rule) (hubapi.Violation, error) {
 	incidents := []hubapi.Incident{}
 	fileCodeSnipCount := map[string]int{}
+	incidentsSet := map[string]struct{}{} // Set of incidents
 	for _, m := range conditionResponse.Incidents {
 		// Exit loop, we don't care about any incidents past the filter.
 		if r.incidentLimit != 0 && len(incidents) == r.incidentLimit {
@@ -442,7 +443,18 @@ func (r *ruleEngine) createViolation(conditionResponse ConditionResponse, rule R
 			incident.Message = templateString
 		}
 
-		incidents = append(incidents, incident)
+		lineNumber, hasLineNumber := incident.Variables["lineNumber"]
+		if !hasLineNumber {
+			lineNumber = -1
+		}
+
+		incidentString := fmt.Sprintf("%s-%s-%d", incident.URI, incident.Message, lineNumber) // Formating a unique string for an incident
+
+		// Adding it to list  and set if no duplicates found
+		if _, isDuplicate := incidentsSet[incidentString]; !isDuplicate {
+			incidents = append(incidents, incident)
+			incidentsSet[incidentString] = struct{}{}
+		}
 	}
 
 	return hubapi.Violation{
