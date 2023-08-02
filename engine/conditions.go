99101d0ab6eb99275fd5abef1dc483b7b40ce8b1
Pranav Gaikwad <pgaikwad@redhat.com>
2023-08-02 08:43:07 -0400

    address feedback
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- engine/conditions.go
+++ engine/conditions.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/konveyor/analyzer-lsp/output/v1/konveyor"
@@ -265,8 +266,10 @@ func (ce ConditionEntry) Evaluate(ctx context.Context, log logr.Logger, condCtx 
 func incidentsToFilepaths(incident []IncidentContext) []string {
 	filepaths := []string{}
 	for _, ic := range incident {
-		if _, err := uri.Parse(string(ic.FileURI)); err == nil {
+		if strings.HasPrefix(string(ic.FileURI), uri.FileScheme) {
 			filepaths = append(filepaths, ic.FileURI.Filename())
+		} else {
+			filepaths = append(filepaths, string(ic.FileURI))
 		}
 	}
 	return filepaths
