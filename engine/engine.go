26ba239288088c0b4dca9376e3ea86f56e2c0e64
Ripul Handoo <ripulhandoo1234@gmail.com>
2023-08-05 15:17:09 +0530

    ✨ Refactor multiline strings to improve human-readability
    Signed-off-by: Ripul Handoo <ripulhandoo1234@gmail.com>

--- engine/engine.go
+++ engine/engine.go
@@ -421,7 +421,15 @@ func (r *ruleEngine) createViolation(conditionResponse ConditionResponse, rule R
 			if err != nil || codeSnip == "" {
 				r.logger.V(6).Error(err, "unable to get code location")
 			} else {
-				incident.CodeSnip = codeSnip
+				// Split the code snippet into lines
+				lines := strings.Split(codeSnip, "\n")
+
+				// Build the properly formatted multiline string
+				var formattedCodeSnip string
+				for _, line := range lines {
+					formattedCodeSnip += "    " + line + "\n"
+				}
+				incident.CodeSnip = "|-\n" + formattedCodeSnip
 			}
 			fileCodeSnipCount[string(m.FileURI)] += 1
 		}
