4f3b1f271e1919a3dcad5a3683e9ab3f34713942
Pranav Gaikwad <pgaikwad@redhat.com>
2023-11-11 08:16:57 -0500

    :bug: fix unit test
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- provider/lib_test.go
+++ provider/lib_test.go
@@ -11,6 +11,7 @@ func TestMultilineGrep(t *testing.T) {
 		name     string
 		filePath string
 		pattern  string
+		window   int
 		want     int
 		wantErr  bool
 	}{
@@ -19,26 +20,37 @@ func TestMultilineGrep(t *testing.T) {
 			filePath: "./testdata/small.xml",
 			pattern:  "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"",
 			want:     2,
+			window:   1,
 			wantErr:  false,
 		},
 		{
 			name:     "multi-line simple pattern",
 			filePath: "./testdata/small.xml",
 			pattern:  "com.fasterxml.jackson.core.*?jackson-core.*",
-			want:     69,
+			want:     68,
+			window:   2,
 			wantErr:  false,
 		},
 		{
 			name:     "multi-line complex pattern",
 			filePath: "./testdata/small.xml",
 			pattern:  "(<groupId>com.fasterxml.jackson.core</groupId>|<artifactId>jackson-core</artifactId>).*?(<artifactId>jackson-core</artifactId>|<groupId>com.fasterxml.jackson.core</groupId>).*",
-			want:     69,
+			want:     68,
+			window:   2,
+			wantErr:  false,
+		},
+		{
+			name:     "multi-line complex pattern",
+			filePath: "./testdata/big.xml",
+			pattern:  "(<groupId>io.konveyor.demo</groupId>|<artifactId>config-utils</artifactId>).*?(<artifactId>config-utils</artifactId>|<groupId>io.konveyor.demo</groupId>).*",
+			want:     664,
+			window:   2,
 			wantErr:  false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := MultilineGrep(context.TODO(), 5, tt.filePath, tt.pattern)
+			got, err := MultilineGrep(context.TODO(), tt.window, tt.filePath, tt.pattern)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MultilineGrep() error = %v, wantErr %v", err, tt.wantErr)
 				return
