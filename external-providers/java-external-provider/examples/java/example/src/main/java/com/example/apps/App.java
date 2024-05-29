06e3bbcc611a9fb366efd41a5740496652a8242d
Pranav Gaikwad <pgaikwad@redhat.com>
2024-05-29 09:06:44 -0400

    :ghost: move inclusion tests to a separate init config and location
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- external-providers/java-external-provider/examples/java/example/src/main/java/com/example/apps/App.java
+++ external-providers/java-external-provider/examples/java/example/src/main/java/com/example/apps/App.java
@@ -1,6 +1,5 @@
 package com.example.apps;
 
-import java.io.File;
 import io.fabric8.kubernetes.api.model.apiextensions.v1beta1.CustomResourceDefinition;
 
 public class App 
@@ -17,11 +16,5 @@ public class App
 
         GenericClass<String> element = new GenericClass<String>("Hello world!");
         element.get();
-
-        // test file usage
-        File file = new File("test");
-        if (file.exists()) {
-            System.out.println("file exists");
-        }
     }
 }
