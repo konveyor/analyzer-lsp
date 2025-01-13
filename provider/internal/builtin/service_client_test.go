5528072ba646d21e3edc7ba53cedd1b9d0592fba
Pranav Gaikwad <pgaikwad@redhat.com>
2025-01-13 17:45:41 -0500

    :ghost: fix linux exclusion and add tests
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- provider/internal/builtin/service_client_test.go
+++ provider/internal/builtin/service_client_test.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"path/filepath"
 	"reflect"
+	"sort"
 	"sync"
 	"testing"
 
+	"github.com/go-logr/logr"
 	"github.com/go-logr/logr/testr"
 	"github.com/konveyor/analyzer-lsp/engine"
 	"github.com/konveyor/analyzer-lsp/provider"
+	"gopkg.in/yaml.v2"
 )
 
 func newLocationForLine(line float64) provider.Location {
@@ -133,6 +136,434 @@ func BenchmarkRunOSSpecificGrepCommand(b *testing.B) {
 		}
 		runOSSpecificGrepCommand("Apache License 1.1",
 			path,
-			provider.ProviderContext{Template: map[string]engine.ChainTemplate{}})
+			provider.ProviderContext{Template: map[string]engine.ChainTemplate{}}, logr.Discard())
+	}
+}
+
+func Test_builtinServiceClient_Evaluate_InclusionExclusion(t *testing.T) {
+	baseLocation := filepath.Join(".", "testdata", "search_scopes")
+	baseLocation, err := filepath.Abs(baseLocation)
+	if err != nil {
+		t.Errorf("builtinServiceClient.Evaluate() unable to run tests, cannot get absolute file path for base location err = %v", err)
+		return
+	}
+
+	tests := []struct {
+		name       string
+		capability string
+		// this is what we already had before introducing scopes
+		includedPathsFromConfig []string
+		condition               builtinCondition
+		chainTemplate           engine.ChainTemplate
+		wantFilePaths           []string
+		wantErr                 bool
+	}{
+		{
+			name:                    "(Filecontent) No include, no exclude",
+			capability:              "filecontent",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.txt"),
+				filepath.Join("dir_a", "a.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.txt"),
+				filepath.Join("dir_b", "dir_a", "ba.properties"),
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "b.txt"),
+			},
+		},
+		{
+			name:       "(Filecontent) Include using the config (legacy inclusion), no exclude",
+			capability: "filecontent",
+			includedPathsFromConfig: []string{
+				"dir_a/",
+			},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.txt"),
+				filepath.Join("dir_a", "a.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.txt"),
+			},
+		},
+		{
+			name:       "(File) Include using the config (legacy inclusion), no exclude",
+			capability: "file",
+			includedPathsFromConfig: []string{
+				"dir_a",
+			},
+			condition: builtinCondition{
+				File: fileCondition{
+					Pattern: ".*.properties",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.properties"),
+			},
+		},
+		{
+			name:       "(XML) Include using the config (legacy inclusion), no exclude",
+			capability: "xml",
+			includedPathsFromConfig: []string{
+				"dir_b",
+			},
+			condition: builtinCondition{
+				XML: xmlCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.xml"),
+				filepath.Join("dir_b", "b.xml"),
+			},
+		},
+		{
+			name:       "(JSON) Include using the config (legacy inclusion), no exclude",
+			capability: "json",
+			includedPathsFromConfig: []string{
+				"dir_b",
+			},
+			condition: builtinCondition{
+				JSON: jsonCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.json"),
+				filepath.Join("dir_b", "b.json"),
+			},
+		},
+		{
+			name:       "(Filecontent) Include using the config (legacy inclusion), with exclude",
+			capability: "filecontent",
+			includedPathsFromConfig: []string{
+				"dir_a/",
+				"dir_b/",
+			},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.properties"),
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "b.txt"),
+			},
+		},
+		{
+			name:       "(File) Include using the config (legacy inclusion), with exclude",
+			capability: "file",
+			includedPathsFromConfig: []string{
+				"dir_a",
+				"dir_b",
+			},
+			condition: builtinCondition{
+				File: fileCondition{
+					Pattern: ".*.properties",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "dir_a", "ba.properties"),
+			},
+		},
+		{
+			name:       "(XML) Include using the config (legacy inclusion), with exclude",
+			capability: "xml",
+			includedPathsFromConfig: []string{
+				"dir_a",
+				"dir_b",
+			},
+			condition: builtinCondition{
+				XML: xmlCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_b"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "dir_b", "ab.xml"),
+				filepath.Join("dir_a", "a.xml"),
+			},
+		},
+		{
+			name:       "(JSON) Include using the config (legacy inclusion), with exclude",
+			capability: "json",
+			includedPathsFromConfig: []string{
+				"dir_a",
+				"dir_b",
+			},
+			condition: builtinCondition{
+				JSON: jsonCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.json"),
+				filepath.Join("dir_b", "b.json"),
+			},
+		},
+		{
+			name:                    "(Filecontent) Include using the scopes, no exclude",
+			capability:              "filecontent",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				Filepaths: []string{
+					filepath.Join("dir_a", "a.txt"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.txt"),
+			},
+		},
+		{
+			name:                    "(File) Include using the scopes, no exclude",
+			capability:              "file",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				File: fileCondition{
+					Pattern: ".*.properties",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				Filepaths: []string{
+					filepath.Join("dir_b", "b.properties"),
+					filepath.Join("dir_b", "dir_a", "ba.properties"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "dir_a", "ba.properties"),
+			},
+		},
+		{
+			name:                    "(XML) Include using the scopes, no exclude",
+			capability:              "xml",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				XML: xmlCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				Filepaths: []string{
+					filepath.Join("dir_a", "a.xml"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.xml"),
+			},
+		},
+		{
+			name:                    "(JSON) Include using the scopes, no exclude",
+			capability:              "json",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				JSON: jsonCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				Filepaths: []string{
+					filepath.Join("dir_b", "dir_a", "ba.json"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.json"),
+			},
+		},
+
+		{
+			name:                    "(Filecontent) Exclude dir, no include",
+			capability:              "filecontent",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "dir_a", "ba.properties"),
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "b.txt"),
+			},
+		},
+		{
+			name:                    "(File) Exclude dir, no include",
+			capability:              "file",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				File: fileCondition{
+					Pattern: "(.*.txt|.*.properties)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a", "dir_b"),
+					filepath.Join(baseLocation, "dir_b", "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.txt"),
+				filepath.Join("dir_a", "a.properties"),
+				filepath.Join("dir_b", "b.properties"),
+				filepath.Join("dir_b", "b.txt"),
+			},
+		},
+		{
+			name:                    "(XML) Exclude dir, no include",
+			capability:              "xml",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				XML: xmlCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_b", "b.xml"),
+				filepath.Join("dir_b", "dir_a", "ba.xml"),
+			},
+		},
+		{
+			name:                    "(JSON) Exclude dir, no include",
+			capability:              "json",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				JSON: jsonCondition{
+					XPath: "//name[text()='Test name']",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					filepath.Join(baseLocation, "dir_b", "dir_a"),
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.json"),
+				filepath.Join("dir_a", "dir_b", "ab.json"),
+				filepath.Join("dir_b", "b.json"),
+			},
+		},
+		{
+			name:                    "(Filecontent) Exclude using pattern",
+			capability:              "filecontent",
+			includedPathsFromConfig: []string{},
+			condition: builtinCondition{
+				Filecontent: fileContentCondition{
+					Pattern: "(fox|app.config.property = .*)",
+				},
+			},
+			chainTemplate: engine.ChainTemplate{
+				ExcludedPaths: []string{
+					".*ba.*",
+					".*.txt",
+				},
+			},
+			wantFilePaths: []string{
+				filepath.Join("dir_a", "a.properties"),
+				filepath.Join("dir_a", "dir_b", "ab.properties"),
+				filepath.Join("dir_b", "b.properties"),
+			},
+		},
+	}
+
+	getAbsolutePaths := func(baseLocation string, relativePaths []string) []string {
+		absPaths := []string{}
+		for _, relPath := range relativePaths {
+			absPaths = append(absPaths, filepath.Join(baseLocation, relPath))
+		}
+		return absPaths
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &builtinServiceClient{
+				config: provider.InitConfig{
+					Location: baseLocation,
+				},
+				log:           testr.New(t),
+				includedPaths: tt.includedPathsFromConfig,
+				locationCache: map[string]float64{},
+				cacheMutex:    sync.RWMutex{},
+			}
+			chainTemplate := engine.ChainTemplate{
+				Filepaths:     getAbsolutePaths(p.config.Location, tt.chainTemplate.Filepaths),
+				ExcludedPaths: tt.chainTemplate.ExcludedPaths,
+			}
+			tt.condition.ProviderContext = provider.ProviderContext{
+				Template: map[string]engine.ChainTemplate{
+					engine.TemplateContextPathScopeKey: chainTemplate,
+				},
+			}
+			conditionInfo, err := yaml.Marshal(&tt.condition)
+			if err != nil {
+				t.Errorf("builtinServiceClient.Evaluate() invalid test case, please check if condition is correct")
+				return
+			}
+			got, err := p.Evaluate(context.TODO(), tt.capability, conditionInfo)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("builtinServiceClient.Evaluate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			gotFilepaths := []string{}
+			for _, fp := range got.Incidents {
+				gotFilepaths = append(gotFilepaths, fp.FileURI.Filename())
+			}
+			wantFilepaths := getAbsolutePaths(p.config.Location, tt.wantFilePaths)
+			sort.Strings(gotFilepaths)
+			sort.Strings(wantFilepaths)
+			if !reflect.DeepEqual(gotFilepaths, wantFilepaths) {
+				t.Errorf("builtinServiceClient.Evaluate() = %v, want %v", gotFilepaths, wantFilepaths)
+			}
+		})
 	}
 }
