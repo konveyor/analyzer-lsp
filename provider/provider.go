e58b0c86e936a187ad163603715dc2fa029337b4
Marek Aufart <maufart@redhat.com>
2025-12-18 16:12:25 +0100

    Update server.go using logLevel
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- provider/provider.go
+++ provider/provider.go
@@ -191,6 +191,9 @@ type InitConfig struct {
 	// Given a pipe name for the init config, we will use that pipe and connect to an already inited provider.
 	Initialized bool `yaml:"initialized" json:"initialized"`
 
+	// LogLevel for the provider logger verbosity
+	LogLevel *int `yaml:"logLevel,omitempty" json:"logLevel,omitempty"`
+
 	// PrepareProgressReporter is an optional interface for reporting progress during Prepare() phase.
 	// If provided, the provider will call ReportProgress() as it processes files during symbol cache population.
 	PrepareProgressReporter PrepareProgressReporter `yaml:"-" json:"-"`
