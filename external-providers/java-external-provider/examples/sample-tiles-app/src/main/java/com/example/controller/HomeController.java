fcdbaf5f63e5af9bfc1ed1ec7a500a896b68508d
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2026-02-25 11:01:39 +0100

    Modify test data
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- external-providers/java-external-provider/examples/sample-tiles-app/src/main/java/com/example/controller/HomeController.java
+++ external-providers/java-external-provider/examples/sample-tiles-app/src/main/java/com/example/controller/HomeController.java
@@ -4,8 +4,7 @@ import com.example.model.B;
 import com.example.service.HomeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Home controller that returns Tiles view names.
