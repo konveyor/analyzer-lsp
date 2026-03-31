01b4b7c4f891e40f155007959bcadc0e00497f77
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2026-03-31 14:19:33 +0200

    STEP 9: add unit tests to service clients
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

---
+++ external-providers/go-external-provider/pkg/go_external_provider/service_client_test.go
@@ -0,0 +1,37 @@
+package go_external_provider_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr"
+	goext "github.com/konveyor/analyzer-lsp/external-providers/go-external-provider/pkg/go_external_provider"
+	"github.com/konveyor/analyzer-lsp/provider"
+)
+
+func TestGoServiceClientBuilderCapabilities(t *testing.T) {
+	builder := &goext.GoServiceClientBuilder{}
+	caps := builder.GetGoServiceClientCapabilities(logr.Discard())
+	if len(caps) == 0 {
+		t.Fatalf("expected non-empty capabilities")
+	}
+}
+
+func TestGoProviderInitErrorPath(t *testing.T) {
+	p := goext.NewGoProvider("generic", logr.Discard(), nil)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	_, _, err := p.Init(ctx, logr.Discard(), provider.InitConfig{
+		ProviderSpecificConfig: map[string]interface{}{
+			"lspServerName": "generic",
+			"lspServerPath": "/definitely/not/a/real/lsp-server",
+			"lspServerArgs": []interface{}{},
+		},
+	})
+	if err == nil {
+		t.Fatalf("expected init error with invalid lspServerPath")
+	}
+}
